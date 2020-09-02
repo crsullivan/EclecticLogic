a8479273833517bb22c5e061eb27b57c8591c5f6
Connor Sullivan <crsullivan@protonmail.com>
2020-09-02 07:54:39 -0700

    routing set up

--- eclecticlogic/src/App.js
+++ eclecticlogic/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Router, Route, Switch } from "react-router-dom";
+import Landing from "./components/Landing.jsx"
+import History from "./utils/History";
 import './App.css';
 
 function App() {
   return (
+    <Router history={History}>
     <div className="App">
-      <header className="App-header">
-        <p>
-        </p>
-        <a>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+      </Switch>
 
-        </a>
-      </header>
     </div>
+    </Router>
   );
 }
 
