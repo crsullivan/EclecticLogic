da4376d7b30faaa6c02c5259e978ccfb17d76884
Connor Sullivan <crsullivan@protonmail.com>
2020-09-03 20:04:14 -0700

    landing done

--- eclecticlogic/src/components/Footer.jsx
+++ eclecticlogic/src/components/Footer.jsx
@@ -5,7 +5,12 @@ const Footer = () => {
 
       return (
         <div className="footer">
-
+          <p className="copyright">Copyright 2020 EclecticLogic</p>
+          <div className="links">
+            <i class="fab fa-instagram fa-2x"></i>
+            <i class="fab fa-linkedin-in fa-2x"></i>
+            <i class="fab fa-twitter fa-2x"></i>
+          </div>
         </div>
       )
     }
--- eclecticlogic/src/components/Landing.jsx
+++ eclecticlogic/src/components/Landing.jsx
@@ -7,26 +7,35 @@ const Landing = () => {
     <div className="mainContainer">
       <div className="header">
         <img className="logo" src={require("../images/E_lecticLogi_ Logo (3).png")}  width="400" height="400" alt="Omega2020 logo" />
-        <h1>wefbhj</h1>
+        <div className="slogan"> 
+          <h3>Beautiful Websites</h3>
+          <h3>Accessibility</h3>
+          <h3>Responsive Design</h3>
+          <h3>Marketing</h3>
+        </div>  
       </div>  
       <div className="body">
         <div className="pair">
           <div className="category">
             <i class="far fa-question-circle fa-5x"></i>
+            <h2>Consulting</h2>
             <p className="description">lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
           </div>
           <div className="category">
             <i class="fas fa-user fa-5x"></i>
+            <h2>Portfolios</h2>
             <p className="description">lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
           </div>
         </div>
         <div className="pair">
           <div className="category">
             <i class="fas fa-poll fa-5x"></i>
+            <h2>Marketing</h2>
             <p className="description">lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
           </div>
           <div className="category">
             <i className="fas fa-seedling fa-5x"> </i>
+            <h2>Growth</h2>
             <p className="description">lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
           </div>
         </div>
