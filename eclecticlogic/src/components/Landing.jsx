1e9c822fd3e746e1afed20e8acefdd99ce088951
Connor Sullivan <crsullivan@protonmail.com>
2020-09-18 12:00:02 -0700

    landing styling done

--- eclecticlogic/src/components/Landing.jsx
+++ eclecticlogic/src/components/Landing.jsx
@@ -15,39 +15,39 @@ const Landing = () => {
         <div className="carouselSection">
             <TopCarousel />
         </div>
-        <div className="callTo">
+        {/* <div className="callTo">
           <h2>Market Yourself</h2>
           <h2>Market Your Business</h2>
           <h2>Market Your Philosophy</h2>  
-        </div>
+        </div> */}
         <div className="icons">
           <div className="iconDiv">
             <div className="header">
               <i class="fas fa-user fa-5x"></i>
               <h1>Portfolios</h1>
             </div>
-            <h3>awegwhiojveoijbwipbjpiwbjwipbjpiowb g jpigqwipg joiqweg iopgh pwi guhbeuiowg hohohseg sdfgn euoghgb noiwgh oihjg owuirghn owergbniwrjgbwsero gn o</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac lectus ut purus pellentesque vulputate. Nam posuere magna quis maximus suscipit. Vivamus id pretium ex. Nam ut libero posuere, lobortis.</p>
           </div>
           <div className="iconDiv">
             <div className="header">
               <i class="far fa-question-circle fa-5x"></i>
               <h1>Consulting</h1>
             </div>
-            <h3>awegwhiojveoijbwipbjpiwbjwipbjpiowb g jpigqwipg joiqweg iopgh pwi guhbeuiowg hohohseg sdfgn euoghgb noiwgh oihjg owuirghn owergbniwrjgbwsero gn o</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac lectus ut purus pellentesque vulputate. Nam posuere magna quis maximus suscipit. Vivamus id pretium ex. Nam ut libero posuere, lobortis.</p>
           </div>
           <div className="iconDiv">
             <div className="header">
               <i class="fas fa-poll fa-5x"></i>
               <h1>Marketing</h1>
             </div>
-            <h3>awegwhiojveoijbwipbjpiwbjwipbjpiowb g jpigqwipg joiqweg iopgh pwi guhbeuiowg hohohseg sdfgn euoghgb noiwgh oihjg owuirghn owergbniwrjgbwsero gn o</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac lectus ut purus pellentesque vulputate. Nam posuere magna quis maximus suscipit. Vivamus id pretium ex. Nam ut libero posuere, lobortis.</p>
           </div>
           <div className="iconDiv">
             <div className="header">
               <i className="fas fa-seedling fa-5x"></i>
               <h1>Growth</h1>
             </div>
-            <h3>awegwhiojveoijbwipbjpiwbjwipbjpiowb g jpigqwipg joiqweg iopgh pwi guhbeuiowg hohohseg sdfgn euoghgb noiwgh oihjg owuirghn owergbniwrjgbwsero gn o</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac lectus ut purus pellentesque vulputate. Nam posuere magna quis maximus suscipit. Vivamus id pretium ex. Nam ut libero posuere, lobortis.</p>
           </div>
         </div>
       </div>
